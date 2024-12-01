f4d45d40970263cfaa06e4d9c229e00404a80e80
Allan Cop <allan.cop@ing.com>
2024-12-01 16:48:56 +0100

    day 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //puzzle1();
+        puzzle2();
+    }
+
+    public static void puzzle1() {
+        Puzzle1.solve();
+    }
+
+    public static void puzzle2(){
+        Puzzle2.solve();
+    }
+}
+
